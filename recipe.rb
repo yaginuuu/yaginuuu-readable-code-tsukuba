d3389f1aa15b1070555d8ea7afbab13bf822a565
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-07-04 14:04:56 +0900

    Remove unnused variable from Recipe.print

--- recipe.rb
+++ recipe.rb
@@ -3,13 +3,11 @@
 class Recipe
   # 出力
   def self.print
-    results = []
     hash = {}
     open(ARGV[0]) do |recipes|
       recipes.each_with_index do |recipe, i|
         hash[:id] = i + 1
         hash[:recipe] = recipe
-        results[i] = hash
         puts "#{hash[:id]}: #{hash[:recipe]}"
       end
     end
