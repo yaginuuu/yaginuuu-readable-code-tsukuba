7c088684929ed84a94192bce29e834e8ad598b24
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2015-07-04 15:24:03 +0900

    spec06

--- recipe.rb
+++ recipe.rb
@@ -6,21 +6,36 @@ class Recipe
     @recipes = []
   end
   
-  def print
+  def print_recipes_all
     @recipes.each do |recipe| 
       puts "#{recipe[:id]}: #{recipe[:name]} #{recipe[:recipe]}"
+    end
+  end
+
+  def print_recipes_by_id(search_id)
+    @recipes.each do |recipe|
+      if search_id == id  
+        puts "#{recipe[:id]}: #{recipe[:name]} #{recipe[:recipe]}"
+      end
     end
   end
 
   def read
     @file.each_line.with_index(1) do |row, id|
       recipe = row.split(" ")
-      @recipes.push({:id => id, :name => recipe[0], :recipe => recipe[1]})
+      @recipes.push({:id => id.to_i, :name => recipe[0], :recipe => recipe[1]})
     end
   end
 end
 
 filename = ARGV[0]
+search_id = ARGV[1]
+
 recipe = Recipe.new(filename)
 recipe.read
-recipe.print
+
+if search_id.nil?
+  recipe.print_recipes_all
+else 
+  recipe.print_recipes_by_id(search_id)
+end
